7b6c907e3591482dc34390bed9ac94ce888f0200
Shu Ding <ds303077135@gmail.com>
2020-08-04 00:32:43 +0800

    Update index.js

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">Next.js!</a> (edited)
       </h1>
 
       <p className="description">
